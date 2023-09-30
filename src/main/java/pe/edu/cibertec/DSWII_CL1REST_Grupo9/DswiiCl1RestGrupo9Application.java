2702c495ad197185872b14e5ea23feb831867bdb
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-09-30 09:47:19 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1REST_Grupo9/DswiiCl1RestGrupo9Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1REST_Grupo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1RestGrupo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1RestGrupo9Application.class, args);
+	}
+
+}
